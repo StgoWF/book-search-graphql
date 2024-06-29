b8ac9007b91b9ca3a1f6a1487722884fb2f9e248
StgoWF <155189000+StgoWF@users.noreply.github.com>
2024-06-29 02:53:14 -0400

    Remove deprecated sslvalidate option and simplify MongoDB connection configuration

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,18 +1,6 @@
-const mongoose = require('mongoose');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    ssl: true,
-    sslValidate: false,
-});
+mongoose.connect(process.env.MONGODB_URI);
 
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
-module.exports = db;
+module.exports = mongoose.connection;
