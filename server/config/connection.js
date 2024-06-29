911250805e8679ca5aa86913481b19e0366c7a15
StgoWF <155189000+StgoWF@users.noreply.github.com>
2024-06-29 02:35:06 -0400

    Updated /connection.js to include SSL options

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  tls: true,  
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+    sslValidate: false,
 });
 
 const db = mongoose.connection;
